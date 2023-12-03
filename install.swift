4649e31e91ce207811693c9d7e0952905e522c94
wangzhizhou <wangzhizhou@bytedance.com>
2023-12-03 20:20:32 +0800

    add some snippet

---
+++ install.swift
@@ -0,0 +1,42 @@
+#!/usr/bin/env swift
+
+import Foundation
+
+
+let snippetDirFileURL = URL(fileURLWithPath: "Snippets")
+
+if let isDir = try snippetDirFileURL.resourceValues(forKeys: [.isDirectoryKey]).isDirectory, isDir == true {
+    
+    let snippets =  try FileManager.default.contentsOfDirectory(atPath: snippetDirFileURL.path())
+    
+    if !snippets.isEmpty {
+        
+        let xcodeUserDataFilePath = NSHomeDirectory().appending("/Library/Developer/Xcode/UserData/CodeSnippets/")
+        
+        let xcodeUserDataFileURL = URL(fileURLWithPath: xcodeUserDataFilePath)
+        
+        let isExist = FileManager.default.fileExists(atPath: xcodeUserDataFileURL.path(percentEncoded: true))
+        
+        if !isExist {
+            
+            try FileManager.default.createDirectory(at: xcodeUserDataFileURL, withIntermediateDirectories: true)
+        }
+        
+        try snippets.forEach { snippetFile in
+            
+            let snippetSourceFilePath = snippetDirFileURL.appending(path: snippetFile).path
+            
+            let snippetTargetFilePath = xcodeUserDataFilePath.appending(snippetFile)
+            
+            if FileManager.default.fileExists(atPath: snippetTargetFilePath) {
+                
+                try FileManager.default.removeItem(atPath: snippetTargetFilePath)
+            }
+            
+            try FileManager.default.copyItem(atPath: snippetSourceFilePath, toPath: snippetTargetFilePath)
+        }
+    }
+}
+
+
+
