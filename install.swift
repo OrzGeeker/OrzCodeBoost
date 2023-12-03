5b1a764bfb909e8ded5dabbb69606f30c2af754e
wangzhizhou <wangzhizhou@bytedance.com>
2023-12-03 22:14:24 +0800

    add template

--- install.swift
+++ install.swift
@@ -4,26 +4,44 @@ import Foundation
 
 try syncSnippets()
 
+try syncFileTemplates()
+
+func syncFileTemplates() throws {
+    
+    let fileTemplateDirName = "File Templates"
+    
+    let fileTemplateDirFileURL = URL(fileURLWithPath: fileTemplateDirName)
+    
+    let xcodeUserDataFileTemplatesFileURL = URL(fileURLWithPath: NSHomeDirectory().appending("/Library/Developer/Xcode/Templates/\(fileTemplateDirName)/"))
+    
+    print("Sync Templates to directory: \(xcodeUserDataFileTemplatesFileURL.path(percentEncoded: false))")
+    
+    try syncDir(from: fileTemplateDirFileURL, to: xcodeUserDataFileTemplatesFileURL)
+    
+}
+
 func syncSnippets() throws {
     
-    let snippetDirFileURL = URL(fileURLWithPath: "Snippets")
+    let snippetDirName = "CodeSnippets"
     
-    let xcodeUserDataFileURL = URL(fileURLWithPath: NSHomeDirectory().appending("/Library/Developer/Xcode/UserData/CodeSnippets/"))
+    let snippetDirFileURL = URL(fileURLWithPath: snippetDirName)
     
-    print("Sync Snippets to directory: \(xcodeUserDataFileURL.path())")
+    let xcodeUserDataCodeSnippetsFileURL = URL(fileURLWithPath: NSHomeDirectory().appending("/Library/Developer/Xcode/UserData/\(snippetDirName)/"))
     
-    try syncDir(from: snippetDirFileURL, to: xcodeUserDataFileURL)
+    print("Sync Snippets to directory: \(xcodeUserDataCodeSnippetsFileURL.path(percentEncoded: false))")
+    
+    try syncDir(from: snippetDirFileURL, to: xcodeUserDataCodeSnippetsFileURL)
 }
 
 func syncDir(from sourceDirFileURL: URL, to destinationDirFileURL: URL) throws {
  
     if let isDir = try sourceDirFileURL.resourceValues(forKeys: [.isDirectoryKey]).isDirectory, isDir == true {
         
-        let sourceDirFiles =  try FileManager.default.contentsOfDirectory(atPath: sourceDirFileURL.path())
+        let sourceDirFiles =  try FileManager.default.contentsOfDirectory(atPath: sourceDirFileURL.path(percentEncoded: false))
         
         if !sourceDirFiles.isEmpty {
             
-            let isExist = FileManager.default.fileExists(atPath: destinationDirFileURL.path(percentEncoded: true))
+            let isExist = FileManager.default.fileExists(atPath: destinationDirFileURL.path(percentEncoded: false))
             
             if !isExist {
                 
@@ -32,9 +50,9 @@ func syncDir(from sourceDirFileURL: URL, to destinationDirFileURL: URL) throws {
             
             try sourceDirFiles.forEach { sourceFile in
                 
-                let sourceFilePath = sourceDirFileURL.appending(path: sourceFile).path
+                let sourceFilePath = sourceDirFileURL.appending(path: sourceFile).path(percentEncoded: false)
                 
-                let destinationFilePath = destinationDirFileURL.path().appending(sourceFile)
+                let destinationFilePath = destinationDirFileURL.path(percentEncoded: false).appending(sourceFile)
                 
                 if FileManager.default.fileExists(atPath: destinationFilePath) {
                     
